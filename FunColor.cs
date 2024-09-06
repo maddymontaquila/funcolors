61e4d695a6061fb4b96263f54351f704cfa2e4f8
Tim Heuer <tim@timheuer.com>
2024-09-06 11:33:34 -0700

    brat summer

--- FunColor.cs
+++ FunColor.cs
@@ -8,4 +8,6 @@ public static class FunColor
     public static Color DotNetPurple2024 => Color.FromUint(0xFF512BD4);
 
     public static Color XamarinBlue2011 => Color.FromUint(0xFF5596D8);
+
+    public static Color BratSummer => Color.FromUint(0xFF8ACE00);
 }
