49ae12d0aafd416fb0fb86eb73a857acfad89eaa
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-09-06 12:24:25 -0500

    Add moar colors
    I took inspiration from:
    
    * https://github.com/jamesmontemagno/vs-red-theme/issues/1
    
    ![image](https://user-images.githubusercontent.com/840039/143777830-eb43c307-2205-4ef5-b95d-c7fce45ff6d2.png)
    
    ![image](https://user-images.githubusercontent.com/840039/143777848-d686b159-3248-4e58-a790-bac807c1ceba.png)

--- FunColor.cs
+++ FunColor.cs
@@ -8,4 +8,40 @@ public static class FunColor
     public static Color DotNetPurple2024 => Color.FromUint(0xFF512BD4);
 
     public static Color XamarinBlue2011 => Color.FromUint(0xFF5596D8);
+
+    /// <summary>
+    /// NeonElectricBlue is the color of a hyperactive blueberry that just discovered energy drinks.
+    /// Pair with NeonVividMagenta to create a vibrant, energetic, panic-inducing color scheme.
+    /// </summary>
+    public static Color NeonElectricBlue => Color.FromUint(0xFF154FEE);
+
+    /// <summary>
+    /// NeonVividMagenta is the color of a party-loving flamingo that refuses to leave the dance floor.
+    /// Pair with NeonElectricBlue to create a vibrant, energetic, panic-inducing color scheme.
+    /// </summary>
+    public static Color NeonVividMagenta => Color.FromUint(0xFFFF08FC);
+
+    /// <summary>
+    /// PukePink is the color of that bubblegum shade you regret eating too much of, resulting in a not-so-pleasant aftermath.
+    /// Pairs best with MushyPeas, perfect for catching attention.
+    /// </summary>
+    public static Color PukePink => Color.FromUint(0xFFFF3AC6);
+
+    /// <summary>
+    /// MushyPeas is the color of those squishy, green peas that somehow always end up on the side of your plate at family dinners.
+    /// Pairs best with PukePink, perfect for catching attention.
+    /// </summary>
+    public static Color MushyPeas => Color.FromUint(0xFF5FA41C);
+
+    /// <summary>
+    /// PainfulRed is the color that screams "Warning! Danger ahead!" and makes your eyes feel like they're running a marathon.
+    /// Pairs best with ShrekGreen, ensuring your design will never go unnoticed.
+    /// </summary>
+    public static Color PainfulRed => Color.FromUint(0xFFFF1A00);
+
+    /// <summary>
+    /// ShrekGreen is the color of a swamp-loving ogre who insists on keeping his lawn immaculate.
+    /// Pairs best with PainfulRed, ensuring your design will never go unnoticed.
+    /// </summary>
+    public static Color ShrekGreen => Color.FromUint(0xFF009B00);
 }
