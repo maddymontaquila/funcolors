6dbd7f93046fd1981b7d88b0552ab5e957823cf7
David Ortinau <david.ortinau@microsoft.com>
2024-09-06 16:48:46 -0500

    Childhood Toy Colors

--- FunColor.cs
+++ FunColor.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Graphics;
 
 namespace FunColors;
 
@@ -6,6 +6,20 @@ namespace FunColors;
 public static class FunColor
 {
     public static Color DotNetPurple2024 => Color.FromUint(0xFF512BD4);
-
     public static Color XamarinBlue2011 => Color.FromUint(0xFF5596D8);
+    public static Color RubberDuckYellow => Color.FromUint(0xFFFFD700);
+    public static Color GameBoyGreen => Color.FromUint(0xFF9BBC0F);
+    public static Color BarbiePink => Color.FromUint(0xFFDA1884);
+    public static Color PotatoHeadBrown => Color.FromUint(0xFF8B4513);
+    public static Color TickleMeElmo => Color.FromUint(0xFFFF3F3F);
+    public static Color LegoRed => Color.FromUint(0xFFB40000);
+    public static Color HotWheelsBlue => Color.FromUint(0xFF005BAC);
+    public static Color NerfOrange => Color.FromUint(0xFFFF6F00);
+    public static Color PlayDohYellow => Color.FromUint(0xFFFBE870);
+    public static Color MyLittlePonyPurple => Color.FromUint(0xFFDDA0DD);
+    public static Color TransformersSilver => Color.FromUint(0xFFC0C0C0);
+    public static Color GIJoeGreen => Color.FromUint(0xFF4B5320);
+    public static Color PowerRangersRed => Color.FromUint(0xFFFF0000);
+    public static Color TeenageMutantNinjaTurtlesGreen => Color.FromUint(0xFF008000);
+    public static Color CareBearsRainbow => Color.FromUint(0xFFFF69B4);
 }
