cffda52709cfaad35a39c1f925a35c3e4d1c4827
Maddy Montaquila <maddymontaquila@New-MadBook-Pro.local>
2024-09-06 00:59:47 -0400

    first commit hehe

---
+++ FunColor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Maui.Graphics;
+
+namespace FunColors;
+
+// All the code in this file is included in all platforms.
+public static class FunColor
+{
+    public static Color DotNetPurple2024 => Color.FromUint(0xFF512BD4);
+
+    public static Color XamarinBlue2011 => Color.FromUint(0xFF5596D8);
+}
